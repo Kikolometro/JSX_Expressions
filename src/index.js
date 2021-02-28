838d7cf4e376b9397697ea980a662ca807c80298
Kikolometro <70818999+Kikolometro@users.noreply.github.com>
2021-02-28 12:14:50 +0100

    Practice Finish

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 
 const name = "Angela";
 const num = 7;
+const currentDate = new Date();
+
+const year = currentDate.getFullYear();
 
 ReactDOM.render(
   <div>
     <h1>Hello {name}!</h1>
     <p>Your lucky number is {num + 2}</p>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
   </div>,
   document.getElementById("root")
 );
